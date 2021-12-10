286e0d32d20528b79828793673ecd7e5ef2a4b04
mjeammet <marie.jeammet@protonmail.com>
2021-12-11 00:05:20 +0100

    add ui form contraints + test issue #2

--- server.py
+++ server.py
@@ -3,24 +3,30 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 
 
 def loadClubs(clubs_json):
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+    with open(clubs_json) as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
 
 
 def loadCompetitions(competitions_json):
-    with open('competitions.json') as comps:
+    with open(competitions_json) as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
 
-def create_app(config):
+def create_app(config={}):
     app = Flask(__name__)
+    app.config.update(config)
     app.secret_key = 'something_special'
-    app.config.from_object(config)
 
-    competitions = loadCompetitions('competitions.json')
-    clubs = loadClubs('clubs.json')
+    competitions_json = 'competitions.json'
+    clubs_json = 'clubs.json'
+    if app.config["TESTING"] == True:
+        competitions_json = 'tests/test_dataset.json'
+        clubs_json = 'tests/test_dataset.json'
+
+    competitions = loadCompetitions(competitions_json)
+    clubs = loadClubs(clubs_json)
 
     @app.route('/')
     def index():
@@ -48,10 +54,18 @@ def create_app(config):
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
         placesRequired = int(request.form['places'])
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-        flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
-
+        placesRemaining = int(competition['numberOfPlaces'])
+        if placesRequired > placesRemaining:
+            flash(f"Cannot book more places than remaining ({placesRemaining}).")
+            return render_template('booking.html',club=club, competition=competition)
+        elif placesRequired > int(club['points']):
+            flash(f"Cannot book more places than points you have ({club['points']}).")
+            return render_template('booking.html',club=club, competition=competition)
+        else:
+            flash('Great-booking complete!')
+            club['points'] = int(club['points']) - placesRequired
+            competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+            return render_template('welcome.html', club=club, competitions=competitions)
 
     # TODO: Add route for points display
 

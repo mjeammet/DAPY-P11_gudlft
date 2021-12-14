5037f34c3cd46764b25fee5aa5f44d7dffba57a2
mjeammet <marie.jeammet@protonmail.com>
2021-12-14 15:40:51 +0100

    add ui constraint when booking > 12 places

--- tests/test_server.py
+++ tests/test_server.py
@@ -13,8 +13,8 @@ class TestAuth:
         assert f'Points available: {initial_club_points-places}' in response.data.decode('utf-8')
         # assert test_competition['numberOfPlaces'] == remainingPlaces-places
 
-    def test_shouldnt_book_more_than_maximum(self, client, test_club, test_competition):
-        places = 13
+    def test_shouldnt_book_more_than_max_per_club(self, client, test_club, test_competition):
+        places = MAX_PER_CLUB+1
         response = client.post('/purchasePlaces', data={"competition": test_competition["name"], "club": test_club['name'], "places": places})
         assert response.status_code == 200
         error_message = f'Cannot book - Trying to book more than maximum allowed ({MAX_PER_CLUB})'

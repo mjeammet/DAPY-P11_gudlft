61775bce50d70a713e44ee1d2a2382d45fe5b0a0
mjeammet <marie.jeammet@protonmail.com>
2021-12-15 19:26:17 +0100

    test and fixes #6

--- server.py
+++ server.py
@@ -56,6 +56,7 @@ def create_app(config={}):
         placesRequired = int(request.form['places'])
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
         flash('Great-booking complete!')
+        club['points'] = int(club["points"]) - placesRequired
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,2 +1,8 @@
 class TestAuth:
-    pass
+    
+    def test_valid_booking_should_update_club_points(self, client, test_club, test_competition):
+        places = 1
+        initial_club_points = int(test_club['points'])
+        response = client.post('/purchasePlaces', data={"competition": test_competition["name"], "club": test_club['name'], "places": places})
+        assert response.status_code == 200
+        assert f'Points available: {initial_club_points-places}' in response.data.decode('utf-8')

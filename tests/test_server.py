22dc4f1d0bfdeacb317da19f1826e5d253d3613a
mjeammet <marie.jeammet@protonmail.com>
2021-12-28 16:06:38 +0100

    unit tests for unknown mail issues (#1)

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,6 +2,23 @@ from datetime import datetime as dt
 
 class TestAuth:
     
+    def test_should_access_home(self, client):
+        assert client.get('/').status_code == 200
+
+    def test_should_redirect_known_email(self, client, test_club):
+        email = test_club["email"]
+        # email = "john@simplylift.co"
+        response = client.post('/showSummary', data={'email' : email})
+        assert response.status_code == 200
+
+    def test_unknown_email_should_return_error(self, client):
+        email = 'certainlynotaknownadress@test.fr'
+        response = client.post('/showSummary', data={'email' : email})
+        assert response.status_code == 302
+
+
+class TestBooking:
+
     def test_valid_booking_should_display_confirmation_message(self, client, test_club, future_competition):
         places = 1
         response = client.post('/purchasePlaces', data={"competition": future_competition["name"], "club": test_club['name'], "places": places})

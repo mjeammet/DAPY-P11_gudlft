ea2c68d6e6a63188d7fb9e6dd28ad2b7fe71063c
mjeammet <marie.jeammet@protonmail.com>
2022-01-03 13:33:59 +0100

    add points board feature (#7)

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from server import create_app, loadClubs, loadCompetitions
-
-@pytest.fixture
-def client():
-    app = create_app({"TESTING": True})
-    with app.test_client() as client:
-        yield client

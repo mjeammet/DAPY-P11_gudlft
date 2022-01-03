62b78215d07939059dad1c4b0d798367c04216b1
mjeammet <marie.jeammet@protonmail.com>
2022-01-03 12:31:25 +0100

    remove broken test and ready for merge

--- tests/test_server.py
+++ tests/test_server.py
@@ -8,15 +8,12 @@ class TestPointsBoard:
     def test_board_correctly_displays_clubs(self, client):
         response = client.get('/pointsBoard')
         assert response.status_code == 200
-        
-        # print(data)
+
         clubs = loadClubs('tests/test_dataset.json')
         data = response.data.decode('utf-8')
         for club in clubs:
             assert club['name'] in data
             assert str(club["points"]) in data
 
-    def test_board_no_club(self, client):
-        clubs = []
-        assert client.get('/pointsBoard').status_code == 200
+    # TODO find a way to test that board is empty when clubs == []
 

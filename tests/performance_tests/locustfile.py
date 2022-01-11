a7b482f2f8d9cbd4621b93a4612bf1b3027d28f2
mjeammet <marie.jeammet@protonmail.com>
2022-01-11 19:20:27 +0100

    add perf tests

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,11 +1,36 @@
 from locust import HttpUser, task
 
-class ProjectPerfTest(HttpUser):
+class PerformanceTest(HttpUser):
     
     @task
     def home(self):
-        self.client.get('/')
+        self.client.get('')
 
-    # @task(3)
-    # def login(self):
-    #     response = self.client.get()
+    @task
+    def pointsBoard(self):
+        self.client.get('pointsBoard')
+
+    @task 
+    def summary(self):
+        data = {"email": "kate@shelifts.co.uk"}
+        self.client.post("showSummary", data=data)
+    
+    @task
+    def booking_page(self):
+        # TODO fails but I don't know why :( 
+        club = "She Lifts"
+        competition = "Fall Classic"
+        self.client.get(f'book/{competition}/{club}', )
+
+    @task
+    def purchasePlaces(self):
+        """Book test. Should not take more than 2 seconds. Default number of users: 6. """
+        # TODO all tests currently failling because test dataset is not loaded and no future competition in production data
+        club = "She Lifts"
+        competition = "Fall Classic"
+        data = {"competition": competition, "club": club, "places": 1}
+        self.client.post('purchasePlaces', data=data)
+
+    @task
+    def login(self):
+        response = self.client.get("logout")
